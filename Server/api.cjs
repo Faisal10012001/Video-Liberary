f6f0d64ba8a5b120f3517f62190b40630c397f3c
Faisal Khan <faisalsania@gmail.com>
2026-05-07 10:55:52 +0530

    Fixed duplicate express declaration

--- Server/api.cjs
+++ Server/api.cjs
@@ -8,100 +8,157 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const express = require("express");
-const app = express();
 
+// 🔥 Home Route
 app.get("/", (req, res) => {
     res.send("Server is running successfully");
-});
-
-app.listen(5050, () => {
-    console.log("Server Started");
 });
 
 
 // 🔹 Get Categories
 app.get("/get-categories", (req, res) => {
+
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
-            database.collection("tblcategories").find({}).toArray()
+
+            database.collection("tblcategories")
+                .find({})
+                .toArray()
                 .then(documents => {
+
                     res.send(documents);
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send({ error: err.message }));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send({ error: err.message }));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
 // 🔹 Get Users
 app.get("/get-users", (req, res) => {
+
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
-            database.collection("tblusers").find({}).toArray()
+
+            database.collection("tblusers")
+                .find({})
+                .toArray()
                 .then(documents => {
+
                     res.send(documents);
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send({ error: err.message }));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send({ error: err.message }));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
 // 🔹 Get Admin
 app.get("/get-admin", (req, res) => {
+
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
-            database.collection("tbladmin").find({}).toArray()
+
+            database.collection("tbladmin")
+                .find({})
+                .toArray()
                 .then(documents => {
+
                     res.send(documents);
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send({ error: err.message }));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send({ error: err.message }));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
 // 🔹 Get All Videos
 app.get("/get-videos", (req, res) => {
+
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
-            database.collection("tblvideos").find({}).toArray()
+
+            database.collection("tblvideos")
+                .find({})
+                .toArray()
                 .then(documents => {
+
                     res.send(documents);
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send({ error: err.message }));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send({ error: err.message }));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
-// 🔥 Get Single Video (FIXED)
+// 🔥 Get Single Video
 app.get("/get-video/:id", (req, res) => {
+
     const id = parseInt(req.params.id);
 
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
 
             database.collection("tblvideos")
-                .find({ videos_id: id })
+                .find({ video_id: id })
                 .toArray()
                 .then(documents => {
+
                     res.send(documents);
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send({ error: err.message }));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send({ error: err.message }));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
@@ -118,17 +175,27 @@ app.post("/register-user", (req, res) => {
 
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
 
-            database.collection("tblusers").insertOne(user)
+            database.collection("tblusers")
+                .insertOne(user)
                 .then(() => {
+
                     console.log("User Registered");
                     res.send({ success: true });
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send({ error: err.message }));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send({ error: err.message }));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
@@ -148,17 +215,27 @@ app.post("/add-video", (req, res) => {
 
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
 
-            database.collection("tblvideos").insertOne(video)
+            database.collection("tblvideos")
+                .insertOne(video)
                 .then(() => {
+
                     console.log("Video Added");
                     res.send({ message: "Video Added" });
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send(err.message));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send(err.message));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
@@ -178,18 +255,27 @@ app.put("/edit-video/:id", (req, res) => {
 
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
 
             database.collection("tblvideos")
-               .updateOne({ videos_id: id }, video)
+                .updateOne({ video_id: id }, video)
                 .then(() => {
+
                     console.log("Video Updated");
                     res.send({ message: "Video Updated" });
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send(err.message));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send(err.message));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
@@ -200,18 +286,27 @@ app.delete("/delete-video/:id", (req, res) => {
 
     mongoClient.connect("mongodb://localhost:27017")
         .then(clientObj => {
+
             const database = clientObj.db("video-project");
 
             database.collection("tblvideos")
-                .deleteOne({ videos_id: id })
+                .deleteOne({ video_id: id })
                 .then(() => {
+
                     console.log("Video Deleted");
                     res.send({ message: "Video Deleted" });
                     clientObj.close();
+
                 })
-                .catch(err => res.status(500).send(err.message));
+                .catch(err => {
+                    res.status(500).send({ error: err.message });
+                });
+
         })
-        .catch(err => res.status(500).send(err.message));
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
+
 });
 
 
